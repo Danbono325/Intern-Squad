fffd73d39df7c170b3d8fdef721200af08fc610d
Daniel Boniface <s1087717@monmouth.edu>
2019-04-06 16:13:54 -0400

    about.html created, routes file altered, appMenu altered

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # get 'welcome/homepage'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#homepage'
+
+  get 'about' => 'pages#about'
 end
