e206ef5fd18cefa11e0e1eb391fb009bbe61bd51
Daniel Boniface <s1087717@monmouth.edu>
2019-04-06 16:31:16 -0400

    Lorem Ipsum generated for about page as well as variables

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,10 @@
 class PagesController < ApplicationController
     def about
-    
+        @title = 'About InternSquad';
+        @content = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Consequat mauris nunc congue nisi vitae. Egestas tellus rutrum tellus pellentesque eu tincidunt tortor. Vestibulum rhoncus est pellentesque elit ullamcorper dignissim. Congue mauris rhoncus aenean vel elit. Id aliquet lectus proin nibh nisl. Ultrices mi tempus imperdiet nulla malesuada. Facilisis magna etiam tempor orci eu lobortis elementum. Porta lorem mollis aliquam ut. Ut consequat semper viverra nam libero justo laoreet. Maecenas volutpat blandit aliquam etiam erat velit. Velit laoreet id donec ultrices tincidunt arcu non sodales. Eu mi bibendum neque egestas congue.
+
+            Fringilla urna porttitor rhoncus dolor purus non enim. Quam vulputate dignissim suspendisse in est ante in nibh mauris. Tristique magna sit amet purus gravida. Pellentesque eu tincidunt tortor aliquam nulla. In nulla posuere sollicitudin aliquam. Arcu cursus vitae congue mauris rhoncus aenean. Ultricies integer quis auctor elit sed. Amet cursus sit amet dictum sit amet. Vitae et leo duis ut diam. Tempus egestas sed sed risus pretium quam vulputate. Aenean et tortor at risus viverra adipiscing at in tellus. In hendrerit gravida rutrum quisque non tellus. Consectetur a erat nam at lectus urna duis convallis convallis. Fringilla phasellus faucibus scelerisque eleifend donec pretium vulputate sapien. Dictum at tempor commodo ullamcorper a lacus vestibulum. Dictumst quisque sagittis purus sit amet volutpat consequat.
+            
+            Elementum sagittis vitae et leo duis ut. Ornare aenean euismod elementum nisi quis eleifend. Massa tincidunt nunc pulvinar sapien. At consectetur lorem donec massa. Varius duis at consectetur lorem donec massa sapien faucibus. Nibh nisl condimentum id venenatis. Et tortor at risus viverra adipiscing at in tellus. Quis risus sed vulputate odio. At ultrices mi tempus imperdiet nulla malesuada. A diam sollicitudin tempor id eu.';
     end
 end
