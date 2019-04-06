d1bfeb8447767ab4ed4e1670ce5ea9a78afe9d38
Daniel Boniface <s1087717@monmouth.edu>
2019-04-06 16:07:00 -0400

    pages controller generated

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  #get 'welcome/homepage'
+  # get 'welcome/homepage'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#homepage'
 end
