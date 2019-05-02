781a9f1154c6399a4368e3e88f37bb727abcbc1e
Daniel Boniface <s1087717@monmouth.edu>
2019-05-02 02:40:10 +0000

    form made consistent

--- app/models/intern.rb
+++ app/models/intern.rb
@@ -1,5 +1,5 @@
 class Intern < ApplicationRecord
-    mount_uploader :resume, AttachmentUploader 
+    mount_uploader :resume, ResumeUploader 
     validates :name, :email, :school, :education, :hours, presence: true
     validates :email, format: { with: URI::MailTo::EMAIL_REGEXP } 
 end
